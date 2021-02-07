ffc8149ed8dc4ab9e4b15c3e8c0acb75cef16828
Madhu Gopinathan <madhu.gopinathan@go-mmt.com>
2021-02-07 18:43:10 +0530

    matching

--- notebooks/causal-inference/convert.sh
+++ notebooks/causal-inference/convert.sh
@@ -3,5 +3,5 @@
 layout="post"
 
 ../notebook_convert.py --nbpath simpsons-paradox-smoking.ipynb --date "2019-10-03" --layout $layout --description "Simpson's Paradox"
-
+../notebook_convert.py --nbpath matching.ipynb --date "2020-08-15" --layout $layout --description "Matching using Propensity Score"
 
